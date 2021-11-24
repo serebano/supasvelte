3bdb103f9a6e72d12fbc63f7a9862e7e68aac4a3
serebano <sergiu.toderascu@gmail.com>
2021-11-24 03:25:15 +0200

    Initialize basic

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../global.css';
+	import Header from '$lib/Header.svelte'
+	import AlertList from '$lib/alert/AlertList.svelte'
+</script>
+
+<Header/>
+<AlertList/>
+<main class="w-full h-screen mx-auto relative px-2 sm:px-6 lg:px-8 bg-gray-100">
+	<slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import SvelteSeo from "svelte-seo";
+	import { handleAlert } from '../lib/alert'
+	import Modal from '$lib/Modal.svelte'
+
+	let isModalOpened = false
+
+	function toggleModal() {
+		isModalOpened = !isModalOpened;
+	}
+
+</script>
+
+<SvelteSeo
+  title="Svelte Starter Kit"
+  description="Svelte with brilliant bells and useful whistles"
+/>
+
+<div>
+	<h1>Svelte Starter Kit!</h1>
+	<button on:click={() => handleAlert({text: 'Hey, Buddy', type: 'success'})}>Show Alert</button>
+	<button on:click={toggleModal}>Show Modal</button>
+	<p>
+		Visit <a class="text-blue-600 underline" href="https://svelte.dev">svelte.dev</a> to learn how to
+		build Svelte apps.
+	</p>
+</div>
+
+{#if isModalOpened}
+	<Modal {toggleModal}>
+		<p>This is in the modal</p>
+		<button>Do something</button>
+	</Modal>
+{/if}
+
+<style style lang="postcss">
+	h1 {
+		@apply text-red-600;
+		@apply text-6xl;
+		@apply font-thin;
+		@apply leading-tight;
+		@apply my-16 mx-auto;
+		@apply max-w-xs;
+	}
+
+	p {
+		@apply max-w-xs;
+		@apply my-8 mx-auto;
+		@apply leading-snug;
+	}
+
+	@screen sm {
+		h1 {
+			@apply max-w-none;
+		}
+
+		p {
+			@apply max-w-none;
+		}
+	}
+</style>
